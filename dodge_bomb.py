11f61fbda80bad5b04d14cf48600fff9ce6d8ebd
Ume <c0b23023ec@edu.teu.ac.jp>
2024-11-26 14:52:50 +0900

    練習4:衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -46,7 +46,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):
+            print("ゲームオーバー")
+            return #ゲームオーバー
         screen.blit(bg_img, [0, 0]) 
+
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
